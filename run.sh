cccf76a72a4b1dde59413ca61e737958ec946025
unknown <francisco.lizana1801@alumnos.ubiobio.cl>
2023-01-25 04:14:06 -0300

    Update

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ docker stop Lizana-front;
 docker rm Lizana-front;
 docker rmi lizana-front;
 docker build -t lizana-front . ;
-docker run --name=Lizana-front -d -p 3318:80 lizana-front;
+docker run --name=Lizana-front -d -p 80:80 lizana-front;
 
