247addae4d83e02ad91f179cce8324fd4bd711a2
unknown <francisco.lizana1801@alumnos.ubiobio.cl>
2023-01-25 04:08:45 -0300

    Update

--- src/app/core/_services/reporte.service.ts
+++ src/app/core/_services/reporte.service.ts
@@ -10,14 +10,14 @@ export class ReporteService {
 
   downloadPDF(body: any): any {
     return this.http.post(
-      'http://pacheco.chillan.ubiobio.cl:8081/api/reporte/general/pdf/',
+      'http://ec2-44-212-72-194.compute-1.amazonaws.com:3000/api/reporte/general/pdf/',
       body,
       { responseType: 'blob' }
     );
   }
   dowloadExcel(body: any): any {
     return this.http.post(
-      'http://pacheco.chillan.ubiobio.cl:8081/api/reporte/estacion/excel/',
+      'http://ec2-44-212-72-194.compute-1.amazonaws.com:3000/api/reporte/estacion/excel/',
       body,
       { responseType: 'blob' }
     );
